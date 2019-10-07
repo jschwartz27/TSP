67ae6affd7fefc903be98a6e699e8ce4bb1b4a76
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2019-10-07 21:22:29 +0200

    Update main.py

--- GA_SA_Hybrid/main.py
+++ GA_SA_Hybrid/main.py
@@ -3,19 +3,36 @@ import genetic_functions as genteik
 
 
 def load_test():
-    with open('test.csv', newline='') as csvfile:
+    with open('Mona_Lisa_1000.csv', newline='') as csvfile:
         coords = csv.reader(csvfile, delimiter=',', quotechar='|')
         return list(map(lambda x: list(map(lambda y: int(y), x)), coords))
 
 
-def main():
-    ML_data = load_test()
-    N = len(ML_data)
-    # print(ML_data[:10]) # .columns.values)
-    chrom_n = 200
-    gen_n = 200
+def load():
+    coords = list()
+    with open('xqf131.csv', newline='') as csvfile:
+        file = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in file:
+            if row[0].isdigit() is False:
+                if row[0] == "OPTIMAL":
+                    optimal = row[2]
+                else:
+                    continue
+            else:
+                coord = (int(row[1]), int(row[2]))
+                coords.append(coord)
 
-    der_Übermensch = genteik.evolve(N, chrom_n, gen_n, ML_data)
+    return coords, optimal
+
+
+def main():
+    data, optimal = load()
+    N = len(data)
+    # print(ML_data[:10]) # .columns.values)
+    chrom_n = 20
+    gen_n = 2000
+
+    der_Übermensch = genteik.evolve(N, chrom_n, gen_n, data, optimal)
     print(der_Übermensch)
 
 if __name__ == '__main__':
