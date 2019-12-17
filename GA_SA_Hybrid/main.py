c9c2781b956b8fd8a31350c77276f1db952298d0
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2019-12-17 18:02:18 +0100

    Update main.py

--- GA_SA_Hybrid/main.py
+++ GA_SA_Hybrid/main.py
@@ -5,12 +5,15 @@ import genetic_functions as genteik
 def main():
     data, optimal = f.load_data()
     params = f.load_parameters()
-    # chrom_n = 500
-    # gen_n = 600
     print("Optimal:: {}\n".format(optimal))
-    der_Übermensch = genteik.evolve(
-        len(data), params["chrom_n"], params["gen_n"], data, optimal)
+    der_Übermensch, fitsDict = genteik.evolve(
+        len(data), params, data, optimal)
     print(der_Übermensch)
+
+    if params["graph_data"] is True:
+        f.show_graph(fitsDict, params)
+    else:
+        quit()
 
 if __name__ == '__main__':
     main()
