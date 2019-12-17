28d9cd73e700986a64e62a5dc070e86b386ad699
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2019-12-17 13:44:37 +0100

    Update main.py

--- GA_SA_Hybrid/main.py
+++ GA_SA_Hybrid/main.py
@@ -1,42 +1,15 @@
-import csv
-import json
+import functions as f
 import genetic_functions as genteik
 
 
-def load_parameters(file_name="parameters.json"):
-    with open(file_name, "r") as file:
-        data = json.load(file)
-
-    return data
-
-
-def load():
-    coords = list()
-    with open('xqf131.csv', newline='') as csvfile:
-        file = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in file:
-            if row[0].isdigit() is False:
-                if row[0] == "OPTIMAL":
-                    optimal = int(row[2])
-                else:
-                    continue
-            else:
-                coord = (int(row[1]), int(row[2]))
-                coords.append(coord)
-
-    return coords, optimal
-
-
 def main():
-    data, optimal = load()
-    params = load_parameters()
-    N = len(data)
-    # print(ML_data[:10]) # .columns.values)
+    data, optimal = f.load_data()
+    params = f.load_parameters()
     # chrom_n = 500
     # gen_n = 600
     print("Optimal:: {}\n".format(optimal))
     der_Übermensch = genteik.evolve(
-        N, params["chrom_n"], params["gen_n"], data, optimal)
+        len(data), params["chrom_n"], params["gen_n"], data, optimal)
     print(der_Übermensch)
 
 if __name__ == '__main__':
