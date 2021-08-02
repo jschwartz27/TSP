e8d0a93417a81194c0d1b058e7b72aaadbfd3fb0
Joshua E. Schwartz <josh.schwartz92@gmail.com>
2021-08-02 20:06:54 +0200

    update Selection Class

--- GA_SA_Hybrid/GA_SA.py
+++ GA_SA_Hybrid/GA_SA.py
@@ -4,7 +4,8 @@ from copy import deepcopy
 from math import exp
 
 import numpy as np
-from typing import Dict, List, Tuple
+from typing import Dict, List
+
 
 # TODO class for genome so it can have best fitness and mean and shit at all times
 # also put create new or fill to max pop or whatever
@@ -13,26 +14,21 @@ from typing import Dict, List, Tuple
 class Chromosome:
 
     def __init__(self, chromosome: np.ndarray, fitness: float):
-        self.chromosome: np.ndarray = chromosome
-        self.fitness: float = fitness
+        self.chromosome = chromosome
+        self.fitness = fitness
 
 
 class Genome:
 
-    best_fitness: float = None
-    mean: float = None
-    standard_deviation: float = None
-
-    def __init__(self, chromosome_n):
-        self.chromosome_n = chromosome_n
-        self.genome: List[Chromosome] = self.__create_chromosomes(chromosome_n)
-
-    def __create_chromosomes(self, chromosome_n):
-        pass
+    def __init__(self, genome: List[Chromosome], fitness_mean_data: Dict, best: Chromosome):
+        self.genome = genome
+        self.best_fitness = best
+        self.fitness_mean_data = fitness_mean_data
 
 
 class GeneticSimulatedAnnealing:
 
+    best: Chromosome = None
     data = {
         "fitness": list(),
         "mean": list()
@@ -40,39 +36,43 @@ class GeneticSimulatedAnnealing:
     temperature: float = None
 
     def __init__(self, city, parameters: Dict):
+
         self.coords: np.ndarray = city.coords  # this list should never change
         self.optimal_distance: int = city.optimal_distance
-        self.chromosome_n: int = parameters["chrom_n"]
-        self.generation_n: int = parameters["gen_n"]
-        self.crossover_p: float = parameters["cross_p"]
-        self.elite_p: float = parameters["elite_p"]
-        self.truncation_p: float = parameters["truncation_p"]
+        self.chromosome_n: int = parameters["general"]["chromosome_n"]
+        self.generation_n: int = parameters["general"]["generation_n"]
 
-        self.selection_parameters = parameters["selection_parameters"] # TODO change yaml and update init
-        self.crossover_parameters = parameters["crossover_parameters"]
+        self.selection_parameters = parameters["selection"]
+        self.crossover_parameters = parameters["crossover"]
+        self.mutation_parameters = parameters["mutation"]
 
         self.genome = self.__create_chroms()
 
-    def run_genetic_simulated_annealing(self):
+    def run_genetic_simulated_annealing(self) -> Genome:
+
+        assert self.chromosome_n % 2 == 0, "Chromosome_n must be even"
+
         self.__append_data()
         for generation in range(self.generation_n):
             self.temperature = self.__annealing_schedule(generation)
 
-            # SELECTION
+            # ### SELECTION ###
             elite, selected_pop = Selection(self.genome, self.chromosome_n, self.temperature,
                                             self.selection_parameters).select()
 
-            # CROSSOVER
+            # ### CROSSOVER ###
             selected_pop_crossed = Crossover(selected_pop, self.chromosome_n, self.temperature, len(self.coords),
                                              self.crossover_parameters).crossover()
 
-            # MUTATION
+            # ### MUTATION ###
             mutated_selected_pop_crossed = Mutation(selected_pop_crossed).mutate()
 
             self.genome = elite + mutated_selected_pop_crossed
 
             self.__append_data()
+
             quit()
+        return Genome(self.genome, self.data, self.best)
 
     def __create_chroms(self) -> List[Chromosome]:
         """return list of Chromosomes sorted by fitness"""
@@ -89,20 +89,20 @@ class GeneticSimulatedAnnealing:
             lambda x: Chromosome(x, self.__eval_distance(x)), chromosomes_raw),
             key=lambda y: y.fitness)
 
-    def __eval_distance(self, chrom: np.ndarray) -> float:
+    def __eval_distance(self, chromosome: np.ndarray) -> float:
         distance = 0
         # This is to add the return to the start point
-        c = np.append(chrom, chrom[0])
+        c = np.append(chromosome, chromosome[0])
 
         for i in range(len(c) - 1):
-            distance += np.linalg.norm(self.coords[c[i]] - self.coords[c[i+1]])
+            distance += np.linalg.norm(self.coords[c[i]] - self.coords[c[i + 1]])
 
         return round(distance, 3)
 
     def __annealing_schedule(self, generation: int) -> float:
         gens_a = self.generation_n * .25
-        eighth = (len(self.coords)/8)/100
-        p = round(eighth * np.exp(((-generation/gens_a) / (len(self.coords)/30))), 2)  # 30 instead of 80
+        eighth = (len(self.coords) / 8) / 100
+        p = round(eighth * np.exp(((-generation / gens_a) / (len(self.coords) / 30))), 2)  # 30 instead of 80
 
         return max(int(p * 100), 1) + 1
 
@@ -124,15 +124,16 @@ class Selection:
     elite: List[Chromosome] = list()
 
     def __init__(self, genome: List[Chromosome], chromosome_n: int, temperature: float, selection_params):
-        self.genome: List[Chromosome] = genome
-        self.chromosome_n: int = chromosome_n
-        self.temperature: float = temperature
+        self.genome = genome
+        self.chromosome_n = chromosome_n
+        self.temperature = temperature
         self.elite_p: float = selection_params["elite_p"]
         self.truncation_p: float = selection_params["truncation_p"]
         self.truncation_amount: float = selection_params["truncation_amount"]
 
     def select(self) -> [List[Chromosome], List[Chromosome]]:
-        """return selected chromosomes NOT sorted by fitness"""
+        """return elite and selected chromosomes NOT sorted by fitness"""
+        assert self.truncation_amount == 0.5, "Truncation amount must be 50%"
 
         # ELITE SELECTION
         if random.random() < self.elite_p:
@@ -144,7 +145,6 @@ class Selection:
 
         # TRUNCATION
         if random.random() < self.truncation_p:
-            # truncates by 50%
             self.__truncation()
 
         # CHOOSE SELECTION FUNCTION
@@ -158,70 +158,62 @@ class Selection:
         return self.elite, selection_function()
 
     def __truncation(self) -> None:
-        assert self.chromosome_n % 2 == 0, "CANNOT TRUNCATE UNEAVEN DNA"
+        assert self.chromosome_n % 2 == 0, "CANNOT TRUNCATE UNEVEN DNA"
 
-        # TODO can truncation amount be any number?
-        dna = self.genome[:int(self.chromosome_n*self.truncation_amount)]
+        dna = self.genome[:int(self.chromosome_n * self.truncation_amount)]
         # this doubles and maintains fitness ordering of chromosomes
-        self.genome = [*dna, *dna]
+        self.genome = [*dna, *dna]  # TODO maybe just better with the zip function
+        self.__sort_genome_fitness()
 
     # ### SELECTION FUNCTIONS ###
 
-    def _linear_rank(self):
-        ns = list(range(1, self.chromosome_n+1))[::-1]
-        probabilities = list(map(lambda x: x/sum(ns), ns))  # normalizes
+    def _linear_rank(self) -> List[Chromosome]:
+        ns = list(range(1, self.chromosome_n + 1))[::-1]
+        probabilities = list(map(lambda x: x / sum(ns), ns))  # normalizes
 
         return random.choices(self.genome, probabilities, k=self.length_to_append)
 
-    def _exponential_rank(self):
-        ns = list(range(1, self.chromosome_n+1))[::-1]
-        e = list(map(lambda x: .5**(5-x), ns))
-        probabilities = list(map(lambda x: x/sum(e), e))
+    def _exponential_rank(self) -> List[Chromosome]:
+        ns = list(range(1, self.chromosome_n + 1))[::-1]
+        e = list(map(lambda x: .5 ** (5 - x), ns))
+        probabilities = list(map(lambda x: x / sum(e), e))
 
         return random.choices(self.genome, probabilities, k=self.length_to_append)
 
-    def _tournament(self):
-        new_chromosomes = list()
+    def _tournament(self) -> List[Chromosome]:
+        new_chromosomes: List[Chromosome] = list()
         for _ in range(self.length_to_append):
-            # this may or may not be a good idea
+            # TODO this may or may not be a good idea
             size = random.choice(range(2, 4))
-            cs = random.sample(self.genome, k=size)
-            # TODO how to get max from list of chroms?
-            new_chromosomes.append(max(cs))
+            chromosomes_tournament = random.sample(self.genome, k=size)
+            # TODO perhaps this could also be subject to the Annealing Schedule
+            new_chromosomes.append(max(chromosomes_tournament, key=lambda x: x.fitness))
 
         return new_chromosomes
 
-    def _roulette(self):
-        # strip to get fitnesses
-        fitnesses = map(lambda chromosome: chromosome.fitness, self.genome)
-        if len(set(fs)) == 1:
-            fs = [1 for _ in range(len(fs))]
-        else:
-            diff = fs[-1]
-            # last chrom has 0 prob of selection, but...
-            fs = list(map(lambda x: x - diff, fs))
-            # here, I bump up last fitness by a notch so not zero
-            fs[-1] = fs[-2]/4
-            fs = list(map(lambda x: x**2, fs))
-
-        total = sum(fs)
-        probabilities = list(map(lambda x: x/total, fs))
+    def _roulette(self) -> List[Chromosome]:
+        fitnesses: List[float] = list(map(lambda chromosome: chromosome.fitness, self.genome))
+        total: float = round(sum(fitnesses), 3)
+        probabilities = list(map(lambda x: x / total, fitnesses))
 
         return random.choices(self.genome, probabilities, k=self.length_to_append)
 
-    def _boltzmann(self):
-        new_chromosomes = list()
+    def _boltzmann(self) -> List[Chromosome]:
+        new_chromosomes: List[Chromosome] = list()
         for _ in range(self.length_to_append):
             cs = random.sample(self.genome, k=2)
-            if cs[0] == max(cs):  # TODO
+            if cs[0].fitness == max(cs, key=lambda x: x.fitness):
                 new_chromosomes.append(cs[0])
             else:
-                if random.random() < exp((cs[0][0]-cs[1][0])/self.temperature):
+                if random.random() < exp((cs[0].fitness - cs[1].fitness) / self.temperature):
                     new_chromosomes.append(cs[0])
                 else:
                     new_chromosomes.append(cs[1])
 
         return new_chromosomes
+
+    def __sort_genome_fitness(self) -> None:
+        self.genome = sorted(self.genome, key=lambda y: y.fitness)
 
 
 class Crossover:
@@ -232,11 +224,11 @@ class Crossover:
         self.chromosome_n = chromosome_n
         self.temperature = temperature
         self.chromosome_l = chromosome_l
-        self.cross_p: float = crossover_params["cross_p"]
+        self.cross_p: float = crossover_params["crossover_p"]
 
     def crossover(self) -> List[Chromosome]:
         random.shuffle(self.genome)
-        half = int(len(self.genome)/2)
+        half = len(self.genome) // 2
 
         cross_dna = flatten(list(map(
             lambda x, y: self.pmx_crossover(x, y, self.chromosome_l, self.cross_p, data),
